e5ec312cee5fe8373c6b79c942ba92a1c8e16618
Bhupender Yadav <85007109+aaffriya@users.noreply.github.com>
2021-12-03 18:28:19 +0530

    Update map.cpp

--- S_T_L/map.cpp
+++ S_T_L/map.cpp
@@ -1,1 +1,18 @@
 /* */
+/* map */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  /* Declare a map */
+  map<int, string> m; /* set key, value data type */
+  m.insert(1, "Bhupender")
+  m[4] = "Hello World";
+
+  /* loop over map */
+  for (auto &item : m)
+  {
+    cout << item.first << ": "<<item.second;
+  }
+}
