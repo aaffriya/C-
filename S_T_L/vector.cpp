3bd28e3b503a426d2cade02cb92986aae88b6b1a
Bhupender Yadav <85007109+aaffriya@users.noreply.github.com>
2021-12-07 17:21:36 +0530

    Update vector.cpp

--- S_T_L/vector.cpp
+++ S_T_L/vector.cpp
@@ -7,19 +7,19 @@ int main()
   /* one dim vector */
   vector<int> v = {55, 8745, 54, 8, 541, 878, 5, 454, 4754, 47};
   for (auto item : v)
-  {
-    cout<<item<<", ";
-  }
+    cout << item << ", ";
 
   /* two dim vector */
   vector<pair<int, int>> V = {{55, 8745}, {54, 8}, {541, 878}, {5, 454}, {4754, 47}};
   for (auto item : V)
-  {
     cout << item.first << " " << item.second << endl;
-  }
 
   /* 
   if use only item then make a copy in item
   if use item with reference (&item) then this is a not copy and reference to orignal item in memory address 
   */
+
+  /* Reverse vector */
+  for (auto it = v.rbegin(); it != v.rend(); ++it)
+    cout << *it << ", ";
 }
